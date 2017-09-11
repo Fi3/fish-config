1685e331b9b3f6c558367a135b7350df6fe0e712
Filippo Merli <filippo.merli@lifeworks.com>
2017-09-11 16:37:49 +0100

    .

--- config.fish
+++ config.fish
@@ -3,10 +3,9 @@
 . "$HOME/.config/fish/functions/aliases.fish"
 
 # nodejs
-set -x NODE_PATH "$HOME/.npm-packages/lib/node_modules" $NODE_PATH
-set -x PATH "$HOME/.npm-packages/bin" $PATH
-set -x PATH "./node_modules/.bin" $PATH
-
+#set -x NODE_PATH "$HOME/.npm-packages/lib/node_modules" $NODE_PATH
+#set -x PATH "$HOME/.npm-packages/bin" $PATH
+#set -x PATH "./node_modules/.bin" $PATH
 
 # editor
 set -x EDITOR "nvim"
@@ -21,3 +20,9 @@ setenv -x LESS_TERMCAP_se (set_color normal)
 setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
 setenv -x LESS_TERMCAP_ue (set_color normal)
 setenv -x LESS_TERMCAP_us (set_color -o green)
+
+# OPAM configuration
+source /Users/filippomerli/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# go
+set -x PATH $PATH /usr/local/go/bin
--- functions/export.fish
+++ functions/export.fish
@@ -2,8 +2,13 @@
 set -x GOPATH $HOME
 
 
-# homebrew
-set -x PATH "$HOME/.linuxbrew/bin" $PATH
+# homebrew (in osx should put bin in ~/usr/local in linux in ~/.linuxbrew
+if test -d $HOME/usr/local/bin
+  set -x PATH "$HOME/usr/local/bin" $PATH
+end
+if test -d $HOME/.linuxbrew/bin
+	set -x PATH "$HOME/.linuxbrew/bin" $PATH
+end
 
 
 # found in my old config file do not know what it is :(
