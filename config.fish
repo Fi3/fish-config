8b6b347e4347f0bc912a68330791a9e39faf2b9a
fi3 <fmerli1@gmail.com>
2018-05-21 10:55:05 +0100

    .

--- config.fish
+++ config.fish
@@ -10,16 +10,6 @@
 # editor
 set -x EDITOR "nvim"
 
-# Colorful man pages
-# from http://pastie.org/pastes/206041/text
-setenv -x LESS_TERMCAP_mb (set_color -o red)
-setenv -x LESS_TERMCAP_md (set_color -o red)
-setenv -x LESS_TERMCAP_me (set_color normal)
-setenv -x LESS_TERMCAP_se (set_color normal)
-setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
-setenv -x LESS_TERMCAP_ue (set_color normal)
-setenv -x LESS_TERMCAP_us (set_color -o green)
-
 # OPAM configuration
 source /Users/filippomerli/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 set -x PATH "$HOME/.opam/system/bin" $PATH
